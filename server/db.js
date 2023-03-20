fe2074f974711e7e5e951da0973111ea0a317b82
AndyFreeL <andy00freelance@gmail.com>
2023-03-20 14:39:17 +0700

    ssl-true db

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user:'teaman',
     password:'LffxTMx6sI7LELp2rAcI5m7qLJwriqDb',
-    host:'dpg-cf4feqo2i3mtifl0ap60-a.frankfurt-postgres.render.com',
+    host:'dpg-cf4feqo2i3mtifl0ap60-a.frankfurt-postgres.render.com?ssl=true',
     port:5432,
     database:'tea_store'
 })
