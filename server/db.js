d8dfc12c789dccb393b4d68a3fa0cca9d9e1ca7e
AndyFreeL <andy00freelance@gmail.com>
2023-03-20 14:02:14 +0700

    123

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user:'postgres',
-    password:'su',
-    host:'localhost',
+    user:'teaman',
+    password:'LffxTMx6sI7LELp2rAcI5m7qLJwriqDb',
+    host:'dpg-cf4feqo2i3mtifl0ap60-a.frankfurt-postgres.render.com',
     port:5432,
     database:'tea_store'
 })
